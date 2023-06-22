1c9617c501ff785a10647ed8fd69f1813211df6e
MorrowCarmody <morrow.carmody@gmail.com>
2023-06-22 17:05:55 -0500

    add outline for labs

---
+++ Lab5_Part1.py
@@ -0,0 +1,14 @@
+# Matt Carmody
+# COSC 6375
+# Lab 5 Part 1
+
+def is_palindrome(string):
+    """Determine if a string is a palindrome."""
+    return
+
+"""
+INSTRUCTIONS:
+Take a string and returns True if it is a palindrome and False otherwise. Use a stack (simulated
+with a list as we did in Section 5.11) to help determine whether a string is a palindrome. Your function
+should ignore case sensitivity (that is, 'a' and 'A' are the same), spaces and punctuations.
+"""
---
+++ Lab5_Part2.py
@@ -0,0 +1,13 @@
+# Matt Carmody
+# COSC 6375
+# Lab 5 Part 2
+
+def is_ordered(sequence):
+    """Determine if the elements of a sequence are in sorted order."""
+    return
+
+"""
+INSTRUCTIONS:
+Receive a sequence and returns True if the elements are in sorted order. Test your function with
+sorted and un-sorted lists, tuples and strings.
+"""
