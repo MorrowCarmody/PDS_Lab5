15db481d8dd2387bd094996d0479e03b8fb8c79e
matt_carmody <matt.carmody@ovsgroup.com>
2023-06-23 02:52:59 -0500

    use stack to evaluate palindrome

--- Lab5_Part1.py
+++ Lab5_Part1.py
@@ -2,34 +2,28 @@
 # COSC 6375
 # Lab 5 Part 1
 
-def is_palindrome(string):
-    """Determine if a string is a palindrome."""
-    string = string.lower()
-    stack = []
+import string
 
-    for letter in string:
+def is_palindrome(input):
+    """Determine if a input is a palindrome."""
+    # convert input to lowercase and remove spaces
+    input = input.lower().replace(' ','')
+    # remove punctuation from input
+    input = input.translate(str.maketrans('', '', string.punctuation))
+    stack = []
+    reversed_input = ''
+
+    for letter in input:
         stack.append(letter)
 
-    word = ''.join(stack)
-    print(word)
+    while(len(stack) > 0):
+        reversed_input += stack.pop()
 
-    stack.reverse()
-    word_reversed = ''.join(stack)
-    print(word_reversed)
-
-    if(word == word_reversed):
-        print('true')
+    if(input == reversed_input):
         return True
     else:
-        print('false')
         return False
 
-is_palindrome('THIS IS A STRING')
-is_palindrome('racecar')
-
-"""
-INSTRUCTIONS:
-Take a string and returns True if it is a palindrome and False otherwise. Use a stack (simulated
-with a list as we did in Section 5.11) to help determine whether a string is a palindrome. Your function
-should ignore case sensitivity (that is, 'a' and 'A' are the same), spaces and punctuations.
-"""
+print(is_palindrome('Don\'t nod!')) # True
+print(is_palindrome('Racecar.'))    # True
+print(is_palindrome('Applejuice'))  # False
